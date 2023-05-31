0d5bd2654cf46ea9b111907741ccc51b186d3ea6
shawn <shawnfeng610@gmail.com>
2023-06-01 01:51:36 +0800

    delete public

--- index.js
+++ index.js
@@ -8,19 +8,7 @@ const videoTranslateRoutes = require('./routes/videoTranslate');
 
 app.use(express.json());
 
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.sendFile('index.html', { root: path.join(__dirname, 'public') });
-});
-
 // use your routes
 app.use('/api', videoTranslateRoutes);
-
-// start the server
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
 
 module.exports = app;
