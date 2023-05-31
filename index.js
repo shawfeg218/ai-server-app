75cf88d72a4dd95c43975ac3096f82f6d1f4e41a
shawn <shawnfeng610@gmail.com>
2023-06-01 01:45:35 +0800

    deploy to vercel

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const videoTranslateRoutes = require('./routes/videoTranslate');
 
 app.use(express.json());
 
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: path.join(__dirname, 'public') });
+});
+
 // use your routes
 app.use('/api', videoTranslateRoutes);
 
@@ -16,3 +22,5 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+module.exports = app;
