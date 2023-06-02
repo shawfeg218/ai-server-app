e54602aa3215dae78f299eded22d0a83deef397e
shawn <shawnfeng610@gmail.com>
2023-06-02 19:21:17 +0000

    add SSL

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // file: index.js
+const https = require('https')
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
 const app = express();
 require('dotenv').config();
 
@@ -13,9 +15,12 @@ app.use(cors());
 // use your routes
 app.use('/api', videoTranslateRoutes);
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/server.shawnweb.site/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/server.shawnweb.site/fullchain.pem')
+};
+
+https.createServer(options, app).listen(443, () => {
+  console.log('Express server listening on port 443');
 });
 
-module.exports = app;
--- services/videoService.js
+++ services/videoService.js
@@ -5,78 +5,89 @@ const ytdl = require('ytdl-core');
 const { OpenAIApi, Configuration } = require('openai');
 
 // exports.downloadVideo = async (videoUrl) => {
-//   const videoId = new URL(videoUrl).searchParams.get('v');
+//   try {
+//     const videoId = new URL(videoUrl).searchParams.get('v');
 
-//   if (!videoId) {
-//     throw new Error('Invalid video url');
+//     if (!videoId) {
+//       throw new Error('Invalid video url');
+//     }
+
+//     const stream = ytdl(videoUrl, { quality: 'highest', format: 'mp4' });
+
+//     const writeStream = fs.createWriteStream(`./tmp/${videoId}.mp4`);
+
+//     stream.pipe(writeStream);
+
+//     return new Promise((resolve, reject) => {
+//       writeStream.on('finish', () => resolve(`./tmp/${videoId}.mp4`));
+//       writeStream.on('error', reject);
+//     });
+//   } catch (error) {
+//     // 处理错误
+//     console.error('Error in downloadVideo:', error);
+//     throw error; // 可选择抛出错误，使调用者能够处理异常情况
 //   }
-
-//   const stream = ytdl(videoUrl, { quality: 'highest', format: 'mp4' });
-
-//   const writeStream = fs.createWriteStream(`./tmp/${videoId}.mp4`);
-
-//   stream.pipe(writeStream);
-
-//   return new Promise((resolve, reject) => {
-//     writeStream.on('finish', () => resolve(`./tmp/${videoId}.mp4`));
-//     writeStream.on('error', reject);
-//   });
 // };
 
 exports.transcribeVideo = async (apiKey, videoUrl) => {
-  // console.log(apiKey);
-  const stream = ytdl(videoUrl, { quality: 'highestaudio', format: 'mp4' });
-  const formData = new FormData();
-  formData.append('file', stream, 'video.mp4');
-  formData.append('model', 'whisper-1');
-  formData.append('response_format', 'srt');
+  try {
+    const stream = ytdl(videoUrl, { quality: 'highestaudio', format: 'mp4' });
+    const formData = new FormData();
+    formData.append('file', stream, 'video.mp4');
+    formData.append('model', 'whisper-1');
+    formData.append('response_format', 'srt');
 
-  const response = await fetch(
-    'https://api.openai.com/v1/audio/transcriptions',
-    {
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
-      },
-      body: formData,
-    }
-  );
+    const response = await fetch(
+      'https://api.openai.com/v1/audio/transcriptions',
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+        body: formData,
+      }
+    );
 
-  const responseText = await response.text();
-  // console.log(responseText);
-  return responseText;
+    const responseText = await response.text();
+    return responseText;
+  } catch (error) {
+    // 处理错误
+    console.error('Error in transcribeVideo:', error);
+    throw error; // 可选择抛出错误，使调用者能够处理异常情况
+  }
 };
 
 exports.translateTranscription = async (apiKey, transcription) => {
-  const prompt =
-    'You are going to be a good translator, capable of judging the situation to derive the most suitable meaning, and translating it into traditional Chinese.';
-  // console.log(transcription);
-  // const test =
-  //   '1\n00:00:00,000 --> 00:00:05,280\nAstro, a tool for building static websites with your favorite JavaScript frameworks,\n\n2\n00:00:05,280 --> 00:00:08,040\nwhile shipping zero JavaScript to the browser.\n\n';
+  try {
+    const prompt =
+      'You are going to be a good translator, capable of judging the situation to derive the most suitable meaning, and translating it into traditional Chinese.';
+    const configuration = new Configuration({ apiKey: apiKey });
+    const openai = new OpenAIApi(configuration);
 
-  const configuration = new Configuration({ apiKey: apiKey });
-  const openai = new OpenAIApi(configuration);
+    const response = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'system',
+          content: prompt,
+        },
+        {
+          role: 'user',
+          content: `翻譯以下內容為繁體中文，但請保留句子的編號與時間的標示: "${JSON.stringify(
+            transcription
+          )}"`,
+        },
+      ],
+    });
 
-  const response = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
-    messages: [
-      {
-        role: 'system',
-        content: prompt,
-      },
-      {
-        role: 'user',
-        content: `翻譯以下內容為繁體中文，但請保留句子的編號與時間的標示: "${JSON.stringify(
-          transcription
-          // test
-        )}"`,
-      },
-    ],
-  });
+    const { data } = response;
+    console.log('Data: ', data);
+    console.log(data.choices[0].message);
 
-  const { data } = response;
-  console.log('Data: ', data);
-  console.log(data.choices[0].message);
-
-  return data.choices[0].message.content;
+    return data.choices[0].message.content;
+  } catch (error) {
+    // 处理错误
+    console.error('Error in translateTranscription:', error);
+    throw error; // 可选择抛出错误，使调用者能够处理异常情况
+  }
 };
