820b14eb524dca13dc8a4e893ba766235a3df6de
shawn <shawnfeng610@gmail.com>
2023-06-16 15:30:31 +0800

    update chat model

--- services/videoService.js
+++ services/videoService.js
@@ -61,7 +61,7 @@ exports.translateTranscription = async (apiKey, transcription) => {
     const openai = new OpenAIApi(configuration);
 
     const response = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
+      model: 'gpt-3.5-turbo-16k',
       messages: [
         {
           role: 'system',
