fa9134bcc21da09a33da7e47b39071197cae8f2b
shawn <shawnfeng610@gmail.com>
2023-06-28 15:14:25 +0800

    add difference way to translate

--- services/videoService.js
+++ services/videoService.js
@@ -54,55 +54,100 @@ exports.translateTranscription = async (apiKey, transcription) => {
   try {
     const prompt =
       'You are going to be a good translator, capable of judging the situation to derive the most suitable meaning, and translating it into traditional Chinese.';
-    const sentencesPerRequest = 85;
+    const sentencesFor16k = 320;
+    const sentencesPerRequest = 80;
     let result = '';
 
     const configuration = new Configuration({ apiKey: apiKey });
     const openai = new OpenAIApi(configuration);
 
-    const transcriptionArray = transcription.split('\n\n');
-    const contentArray = [];
-    let item = '';
-    let index = 0;
+    if (transcription.length > 15000) {
+      const transcriptionArray = transcription.split('\n\n');
+      const contentArray = [];
+      let item = '';
+      let index = 0;
 
-    for (let i = 0; i < transcriptionArray.length; i++) {
-      item += transcriptionArray[i] + '\n\n';
-      index++;
+      for (let i = 0; i < transcriptionArray.length; i++) {
+        item += transcriptionArray[i] + '\n\n';
+        index++;
 
-      if (index === sentencesPerRequest) {
+        if (index === sentencesFor16k) {
+          contentArray.push(item);
+          item = '';
+          index = 0;
+        }
+      }
+
+      if (item !== '') {
         contentArray.push(item);
-        item = '';
-        index = 0;
+      }
+
+      for (let item of contentArray) {
+        const response = await openai.createChatCompletion({
+          model: 'gpt-3.5-turbo-16k',
+          messages: [
+            {
+              role: 'system',
+              content: prompt,
+            },
+            {
+              role: 'user',
+              content: `翻譯以下內容為繁體中文，但請保留句子的編號與時間的標示: "${item}"`,
+            },
+          ],
+        });
+
+        const { data } = response;
+        // console.log('Data: ', data);
+        // console.log(data.choices[0].message);
+        console.log(data.usage);
+
+        result += data.choices[0].message.content;
+      }
+    } else {
+      const transcriptionArray = transcription.split('\n\n');
+      const contentArray = [];
+      let item = '';
+      let index = 0;
+
+      for (let i = 0; i < transcriptionArray.length; i++) {
+        item += transcriptionArray[i] + '\n\n';
+        index++;
+
+        if (index === sentencesPerRequest) {
+          contentArray.push(item);
+          item = '';
+          index = 0;
+        }
+      }
+
+      if (item !== '') {
+        contentArray.push(item);
+      }
+
+      for (let item of contentArray) {
+        const response = await openai.createChatCompletion({
+          model: 'gpt-3.5-turbo',
+          messages: [
+            {
+              role: 'system',
+              content: prompt,
+            },
+            {
+              role: 'user',
+              content: `翻譯以下內容為繁體中文，但請保留句子的編號與時間的標示: "${item}"`,
+            },
+          ],
+        });
+
+        const { data } = response;
+        // console.log('Data: ', data);
+        // console.log(data.choices[0].message);
+        console.log(data.usage);
+
+        result += data.choices[0].message.content;
       }
     }
-
-    if (item !== '') {
-      contentArray.push(item);
-    }
-
-    for (let item of contentArray) {
-      const response = await openai.createChatCompletion({
-        model: 'gpt-3.5-turbo-16k',
-        messages: [
-          {
-            role: 'system',
-            content: prompt,
-          },
-          {
-            role: 'user',
-            content: `翻譯以下內容為繁體中文，但請保留句子的編號與時間的標示: "${item}"`,
-          },
-        ],
-      });
-
-      const { data } = response;
-      // console.log('Data: ', data);
-      // console.log(data.choices[0].message);
-      console.log(data.usage);
-
-      result += data.choices[0].message.content;
-    }
-
     return (result += '\n\n\n');
   } catch (error) {
     console.error('Error in translateTranscription:', error);
