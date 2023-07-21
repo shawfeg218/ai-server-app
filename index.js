be85213bb4ec573efcbf1713db176e6890ef4935
shawn <shawnfeng610@gmail.com>
2023-07-22 01:07:49 +0800

    add socket server

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // file: index.js
 const https = require('https');
+const http = require('http');
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
+const { Server } = require('socket.io');
 const app = express();
 require('dotenv').config();
 
@@ -20,10 +22,42 @@ const options = {
   cert: fs.readFileSync('/etc/letsencrypt/live/server.shawnweb.site/fullchain.pem'),
 };
 
-https.createServer(options, app).listen(443, () => {
+const server = https.createServer(options, app);
+// const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: '*', // 允許所有源
+    methods: ['GET', 'POST'], // 允許的請求方式
+  },
+});
+
+let clientsCount = 0;
+
+io.on('connection', (socket) => {
+  clientsCount++;
+  console.log('Number of clients connected: ', clientsCount);
+
+  socket.on('lock_page', (path) => {
+    console.log('lock_page event received with path:', path);
+    socket.broadcast.emit('lock_page_student', path);
+  });
+
+  socket.on('unlock_page', () => {
+    console.log('unlock_page event received');
+    socket.broadcast.emit('unlock_page_student');
+  });
+
+  socket.on('disconnect', () => {
+    clientsCount--;
+    console.log('Number of clients connected: ', clientsCount);
+  });
+});
+
+server.listen(443, () => {
   console.log('Express server listening on port 443');
 });
 
-// app.listen(5000, () => {
+// server.listen(5000, () => {
 //   console.log('Express server listening on port 5000');
 // });
