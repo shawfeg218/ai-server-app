f9418e2619f7d585b601c002769d456fcc94398b
shawn <shawnfeng610@gmail.com>
2023-11-16 12:35:56 +0800

    add v1 in api route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,47 +1,47 @@
 // file: routes\apiRoutes.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const videoController = require('../controllers/videoController');
-const audioChatController = require('../controllers/audioChatController');
-const esp32Function = require('../mqtt/esp32Function');
-const multer = require('multer');
+const videoController = require("../controllers/videoController");
+const audioChatController = require("../controllers/audioChatController");
+const esp32Function = require("../mqtt/esp32Function");
+const multer = require("multer");
 const upload = multer();
 
-router.post('/transcript-audio', upload.single('file'), audioChatController.transcriptAudio);
+router.post("/v1/transcript-audio", upload.single("file"), audioChatController.transcriptAudio);
 
-router.post('/audio-chat', audioChatController.audioChat);
+router.post("/v1/audio-chat", audioChatController.audioChat);
 
-router.post('/tti', audioChatController.tti);
+router.post("/v1/tti", audioChatController.tti);
 
-router.post('/tts', audioChatController.tts);
+router.post("/v1/tts", audioChatController.tts);
 
-router.post('/video-translate', videoController.translateVideo);
+router.post("/v1/video-translate", videoController.translateVideo);
 
-router.post('/content-learning', videoController.contentLearning);
+router.post("/v1/content-learning", videoController.contentLearning);
 
 // mqtt
-router.post('/reset-wifi', esp32Function.resetWifi);
+router.post("/v1/reset-wifi", esp32Function.resetWifi);
 
-router.post('/set-axis-angle', esp32Function.setAxisAngle);
-router.post('/T-set-axis-angle', esp32Function.TsetAxisAngle);
+router.post("/v1/set-axis-angle", esp32Function.setAxisAngle);
+router.post("/v1/T-set-axis-angle", esp32Function.TsetAxisAngle);
 
-router.post('/correct-act', esp32Function.correctAct);
-router.post('/T-correct-act', esp32Function.TcorrectAct);
+router.post("/v1/correct-act", esp32Function.correctAct);
+router.post("/v1/T-correct-act", esp32Function.TcorrectAct);
 
-router.post('/wrong-act', esp32Function.wrongAct);
-router.post('/T-wrong-act', esp32Function.TwrongAct);
+router.post("/v1/wrong-act", esp32Function.wrongAct);
+router.post("/v1/T-wrong-act", esp32Function.TwrongAct);
 
-router.post('/grab-act', esp32Function.grabAct);
-router.post('/T-grab-act', esp32Function.TgrabAct);
+router.post("/v1/grab-act", esp32Function.grabAct);
+router.post("/v1/T-grab-act", esp32Function.TgrabAct);
 
-router.post('/reset-arm', esp32Function.resetArm);
-router.post('/T-reset-arm', esp32Function.TresetArm);
+router.post("/v1/reset-arm", esp32Function.resetArm);
+router.post("/v1/T-reset-arm", esp32Function.TresetArm);
 
-router.post('/speak-act', esp32Function.speakAct);
-router.post('/T-speak-act', esp32Function.TspeakAct);
+router.post("/v1/speak-act", esp32Function.speakAct);
+router.post("/v1/T-speak-act", esp32Function.TspeakAct);
 
-router.post('/unsubscribe-topic', esp32Function.unsubscribeTopic);
-router.post('/get-angles', esp32Function.getAngles);
-router.post('/get-esp32Status', esp32Function.getEsp32Status);
+router.post("/v1/unsubscribe-topic", esp32Function.unsubscribeTopic);
+router.post("/v1/get-angles", esp32Function.getAngles);
+router.post("/v1/get-esp32Status", esp32Function.getEsp32Status);
 
 module.exports = router;
