10a5fbdaccd3de16768eb844d6df051debffc629
shawn <shawnfeng610@gmail.com>
2023-11-16 13:57:06 +0000

    use nginx for https and domain name

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // file: index.js
-const https = require('https');
-// const http = require('http');
+const http = require('http');
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
@@ -16,22 +15,12 @@ app.use(cors());
 // use your routes
 app.use('/api', apiRoutes);
 
-const options = {
-  key: fs.readFileSync(`/etc/letsencrypt/live/${process.env.DOMAIN_NAME}/privkey.pem`),
-  cert: fs.readFileSync(`/etc/letsencrypt/live/${process.env.DOMAIN_NAME}/fullchain.pem`),
-};
-
-const server = https.createServer(options, app);
-// const server = http.createServer(app);
+const server = http.createServer(app);
 
 // socket.io
 const { createSocketIOServer } = require('./socketIO/socketIO');
 createSocketIOServer(server);
 
-server.listen(443, () => {
-  console.log('Express server listening on port 443');
+server.listen(5000, () => {
+  console.log('Express server listening on port 5000');
 });
-
-// server.listen(5000, () => {
-//   console.log('Express server listening on port 5000');
-// });
